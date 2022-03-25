8ec15c378a87f798cd7666b97d8ebd22f5696df6
marty-zhu <marty.zhu@gmail.com>
2022-03-24 20:27:03 -0700

    solved exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+    let c = ((f - 32) * 5 / 9).toFixed(1);
+    return Number(c);
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+    let f = (c * 9 / 5 + 32).toFixed(1);
+    return Number(f);
 };
 
 // Do not edit below this line

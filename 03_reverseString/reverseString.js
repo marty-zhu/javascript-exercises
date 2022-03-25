0c3e8fe2dc16d89509802059021fa1981e260099
marty-zhu <marty.zhu@gmail.com>
2022-03-24 17:16:27 -0700

    solved exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = [...string];
+    let arrReversed = arr.reverse();
+    return arrReversed.join('');
 };
 
 // Do not edit below this line

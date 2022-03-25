6796a922598d1ab8fef48c9aaae926de58ca4679
marty-zhu <marty.zhu@gmail.com>
2022-03-24 19:31:19 -0700

    solved exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || (typeof(a) != 'number')) {
+        return 'ERROR';
+    } else if (b < 0 || (typeof(b) != 'number')) {
+        return 'ERROR';
+    };
 
+    if (a > b) {
+        [a, b] = [b, a];
+    };
+
+    let arr = [];
+    for (let i = a; i < b + 1; i++) {
+        arr.push(i);
+    };
+    let sum = arr.reduce((allPrevious, current) => allPrevious + current);
+    return sum;
 };
 
 // Do not edit below this line

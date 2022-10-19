e3cff432e33221c3078f1bb365ba6a2c41b5ee4f
marty-zhu <marty.zhu@gmail.com>
2022-10-19 18:27:42 -0400

    added solution code to exe 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let seq = [1, 1];
+    while (n > seq.length) {
+        seq.push(seq.at(-1) + seq.at(-2));
+    };
+    return seq.at(-1);
 };
+
+// fibonacci(4);
 
 // Do not edit below this line
 module.exports = fibonacci;

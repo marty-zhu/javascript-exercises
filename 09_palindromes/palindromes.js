9a440fdaf54d0d57870585dc5b94acd5cc694837
marty-zhu <marty.zhu@gmail.com>
2022-10-19 18:01:43 -0400

    submitting code for exe 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let noPunctuation = str.replace(/[\.,\!\?\s]/g, "");
+    let strArr = [...noPunctuation];
+    let reversed = strArr.reverse();
+    return strArr == reversed;
 };
+
+// console.log(palindromes('racecar!'));
 
 // Do not edit below this line
 module.exports = palindromes;

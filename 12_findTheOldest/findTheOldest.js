c79f10798899d05e9ed16dbc9b52766cfed586c3
marty-zhu <marty.zhu@gmail.com>
2022-10-20 11:45:49 -0400

    added solution code to exe 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const thisYear = new Date().getFullYear();
+    let Oldest = people.sort((firstPerson, secondPerson) => {
+        let firstAge = (firstPerson.yearOfDeath || thisYear) - firstPerson.yearOfBirth;
+        let secondAge = (secondPerson.yearOfDeath || thisYear) - secondPerson.yearOfBirth;
+        return firstAge >= secondAge ? -1:1;
+    });
+    return Oldest[0];
 };
 
 // Do not edit below this line
